5820018122efc64f3248c58ec9411969a2b94bac
ertnerpeter <ertnerpeter@gmail.com>
2024-03-27 09:18:23 +0100

    Update hello-openshift-for-developers.go

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift for Developers! modified"
+		response = "Hello OpenShift for Developers! modified2"
 	}
 
 	fmt.Fprintln(w, response)
